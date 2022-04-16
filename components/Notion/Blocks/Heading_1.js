681a4be73c5295185abcc63725bd54f4f36f9891
rgq <673227233@qq.com>
2022-04-16 22:41:04 +0800

    heading bold

--- components/Notion/Blocks/Heading_1.js
+++ components/Notion/Blocks/Heading_1.js
@@ -3,7 +3,7 @@ import RichText, {colors} from "../Common/RichText";
 const Heading_1 = ({ heading_1 }) => {
   const { rich_text, color } = heading_1;
   return (
-    <h1 className={`${colors[color]} text-3xl mt-12 mb-6`}>
+    <h1 className={`${colors[color]} text-3xl mt-12 mb-6 font-bold`}>
       {rich_text.map((i, index) => (
         <RichText rich_text={i} key={index} />
       ))}
--- components/Notion/Blocks/Heading_2.js
+++ components/Notion/Blocks/Heading_2.js
@@ -3,7 +3,7 @@ import RichText, { colors } from '../Common/RichText';
 const Heading_2 = ({ heading_2 }) => {
   const { rich_text, color } = heading_2;
   return (
-    <h2 className={`${colors[color]} text-2xl mt-8 mb-4`}>
+    <h2 className={`${colors[color]} text-2xl mt-8 mb-4 font-bold`}>
       {rich_text.map((i, index) => (
         <RichText rich_text={i} key={index} />
       ))}
