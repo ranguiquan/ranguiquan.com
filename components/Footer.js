0b3b6c4695d88c7b980a271bc0fe2f21ef39b439
rgq <673227233@qq.com>
2022-04-23 21:00:40 +0800

    adjust footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,24 @@
+import { Nextdotjs, Notion, Vercel } from '@icons-pack/react-simple-icons';
+
 export default function Footer({}) {
   return (
-    <footer className="flex justify-center">
-      <div className='flex justify-between w-full lg:w-[1024px] p-2 border-t-2'>
-        <span className=" text-sm text-paragraph">copy right</span>
-        <span className=" text-sm text-paragraph">ranguiquan</span>
+    <footer className='flex flex-col justify-center items-center text-rich-gray text-sm'>
+      <div className='flex justify-center w-full lg:w-[1024px] border-t-2 pt-4'>
+        <span className=''>{`©️ 2021 - ${new Date().getFullYear()}`}</span>
+        <span className='ml-2'>by ranguiquan</span>
+      </div>
+      <div className='flex justify-center items-center space-x-2 mt-2'>
+        <span>Powered by</span>
+        <a href='https://nextjs.org/'>
+          <Nextdotjs size={14} />
+        </a>
+        <a href='https://www.notion.so/'>
+          <Notion size={14} />
+        </a>
+        <span>hosted in</span>
+        <a href='https://vercel.com/'>
+          <Vercel size={14} />
+        </a>
       </div>
     </footer>
   );
