0d89982309162b74606fa38439cc21c92b6b4c8b
rgq <673227233@qq.com>
2022-04-23 21:09:02 +0800

    fix building error

--- components/Post/PostAuthor.js
+++ components/Post/PostAuthor.js
@@ -3,7 +3,7 @@ export function PostAuthor({ author, date }) {
   // TODO: make it better !!!
   return (
     <div className=' space-x-2 flex justify-center font-semibold pt-2'>
-      {author.map((item) => (
+      {author?.map((item) => (
         <span key={item.id}>{item.name}</span>
       ))}
       <span>{date.toLocaleDateString()}</span>
