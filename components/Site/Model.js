464211dcfc9e1134f1c6f2f7123c3b30957eb7bb
ranguiquan <ranguiquan@gmail.com>
2022-04-26 09:12:29 +0800

    model user select none

--- components/Site/Model.js
+++ components/Site/Model.js
@@ -7,9 +7,9 @@ export const Model = ({ isModelVisible, modelContent }) => {
     <div
       className={`flex justify-center items-center ${
         isModelVisible
-          ? 'model-transition-visible cursor-pointer'
+          ? 'model-transition-visible cursor-zoom-out'
           : 'model-transition-invisible cursor-zoom-in '
-      } fixed top-0 left-0 z-20 w-screen h-screen bg-gray-700/50 backdrop-blur-md overflow-scroll`}
+      } fixed top-0 left-0 z-20 w-screen h-screen bg-gray-700/50 backdrop-blur-md overflow-scroll select-none`}
       onClick={(e) => modelDispatch({ type: 'CLEAR_MODEL' })}>
       {modelContent}
     </div>
