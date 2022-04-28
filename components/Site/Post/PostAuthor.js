4afa566ce4b1011d675589301c341e291f20aa0a
ranguiquan <ranguiquan@gmail.com>
2022-04-29 00:25:09 +0800

    improve date on page
    add isPageHidden property. change date display format.

--- components/Site/Post/PostAuthor.js
+++ components/Site/Post/PostAuthor.js
@@ -1,12 +1,11 @@
 import React from 'react';
-export function PostAuthor({ author, date }) {
+export function PostAuthor({ author }) {
   // TODO: make it better !!!
   return (
-    <div className=' space-x-2 flex justify-center font-semibold pt-2'>
+    <>
       {author?.map((item) => (
         <span key={item.id}>{item.name}</span>
       ))}
-      <span>{date.toLocaleDateString()}</span>
-    </div>
+    </>
   );
 }
